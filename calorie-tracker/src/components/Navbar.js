92f4b49974c38458eaa72703b1bbd397a3641c7f
Sean Kelman <seanrkelman@gmail.com>
2024-01-04 22:08:15 -0800

    added navbar to frontend

--- calorie-tracker/src/components/Navbar.js
+++ calorie-tracker/src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
     <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">SEAN R KELMAN</Link>
+                <Link to="/" className="navbar-logo">Calorie Tracker</Link>
                 <div className = 'menu-icon' onClick={handleClick}>
                     <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
